9b44c85325b647e57a27c43209ae130f2b3da036
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-12-15 05:47:04 +0400

    020 Separated the state of the forms into three states

--- src/components/PopupForm.js
+++ src/components/PopupForm.js
@@ -4,41 +4,55 @@ import { CurrentUserContext } from '../contexts/CurrentUserContext';
 function PopupForm(props) {
   const { formsMap, contentsConfig: { name: popupName, title }, updateUserData } = props; // popupName введено вместо name для исключения конфликта с CurrentUserContext
   const { about, avatar, name } = useContext(CurrentUserContext);
-  const [values, setValues] = useState({
+  const [editProfileValues, setEditProfileValues] = useState({
     userName: '',
     userAbout: '',
+  });
+  const [changePhotoValues, setChangePhotoValues] = useState({
+    avatar: '',
+  });
+  const [newPlaceValues, setNewPlaceValues] = useState({
     placeName: '',
     placeImagelink: '',
-    avatar: '',
   });
 
   useEffect(() => {
-    setValues((oldValues) => {
-      return {
-        ...oldValues,
-        userName:
-        name,
-        userAbout:
-        about,
-        avatar
-      };
+    setEditProfileValues({
+      userName: name,
+      userAbout: about,
     });
+    setChangePhotoValues({ avatar });
   }, [name, about, avatar]);
 
   function handleInputChange(event) {
     const { name, value } = event.target;
 
-    // setValues((oldValues) => {
-    //   return {
-    //     ...oldValues,
-    //     [name]: value,
-    //   };
-    // });
-    setValues({
-      ...values,
-      [name]: value,
-    });
-    // console.log('values', values); // Почему этот консоль-лог выдает неактуальное состояние объекта?
+    switch (popupName) {
+      case formsMap.editProfile.name:
+        setEditProfileValues({
+          ...editProfileValues,
+          [name]: value,
+        });
+        break;
+      case formsMap.changePhoto.name:
+        setChangePhotoValues({
+          ...changePhotoValues,
+          [name]: value,
+        });
+        break;
+      case formsMap.newPlace.name:
+        setNewPlaceValues({
+          ...newPlaceValues,
+          [name]: value,
+        });
+        break;
+      default:
+    }
+    // console.log(
+    //   'editProfileValues', editProfileValues, '\n',
+    //   'changePhotoValues', changePhotoValues, '\n',
+    //   'newPlaceValues', newPlaceValues
+    // ); // Почему этот консоль-лог выдает неактуальное состояние объекта?
   }
 
   function handleSubmit(event) {
@@ -53,21 +67,21 @@ function PopupForm(props) {
       <form className="popup__form" name={popupName} onSubmit={handleSubmit} noValidate>
         {popupName === formsMap.editProfile.name &&
           (<>
-            <input type="text" name="userName" value={values.userName} id="user-name" className="popup__input" onChange={handleInputChange} placeholder="Имя" required minLength={2} maxLength={30} />
+            <input type="text" name="userName" value={editProfileValues.userName} id="user-name" className="popup__input" onChange={handleInputChange} placeholder="Имя" required minLength={2} maxLength={30} />
             <span className="popup__error" id="user-name-error" />
-            <input type="text" name="userAbout" value={values.userAbout} id="user-about" className="popup__input" onChange={handleInputChange} placeholder="О себе" required minLength={2} maxLength={30} />
+            <input type="text" name="userAbout" value={editProfileValues.userAbout} id="user-about" className="popup__input" onChange={handleInputChange} placeholder="О себе" required minLength={2} maxLength={30} />
             <span className="popup__error" id="user-about-error" />
           </>)}
         {popupName === formsMap.newPlace.name &&
           (<>
-            <input type="text" name="placeName" value={values.placeName} id="place-name" className="popup__input" onChange={handleInputChange} placeholder="Название" required minLength={2} maxLength={30} />
+            <input type="text" name="placeName" value={newPlaceValues.placeName} id="place-name" className="popup__input" onChange={handleInputChange} placeholder="Название" required minLength={2} maxLength={30} />
             <span className="popup__error" id="place-name-error" />
-            <input type="url" name="placeImagelink" value={values.placeImagelink} id="place-link" className="popup__input" onChange={handleInputChange} placeholder="Ссылка на картинку" required />
+            <input type="url" name="placeImagelink" value={newPlaceValues.placeImagelink} id="place-link" className="popup__input" onChange={handleInputChange} placeholder="Ссылка на картинку" required />
             <span className="popup__error" id="place-link-error" />
           </>)}
         {popupName === formsMap.changePhoto.name &&
           (<>
-            <input type="url" name="avatar" value={values.avatar} id="avatar" className="popup__input" onChange={handleInputChange} placeholder="Ссылка на аватар" required />
+            <input type="url" name="avatar" value={changePhotoValues.avatar} id="avatar" className="popup__input" onChange={handleInputChange} placeholder="Ссылка на аватар" required />
             <span className="popup__error" id="avatar-error" />
           </>)}
         <button type="submit" className="button popup__button" >Сохранить</button> {/* disabled */}
