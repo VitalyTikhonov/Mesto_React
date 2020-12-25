103ff81acad535b5a26f5156715091a8e7ab3c89
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-12-25 13:04:47 +0400

    037 Favicon. Get one's own cards API method

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 function NavBar () {
   return (
     <nav className="nav-bar">
-      <NavLink exact to="/" className="nav-bar__link link" activeClassName="nav-bar__link_active" >Галерея</NavLink>
+      <NavLink exact to="/" className="nav-bar__link link" activeClassName="nav-bar__link_active" >Главная</NavLink>
       <NavLink to="/user-profile" className="nav-bar__link link" activeClassName="nav-bar__link_active" >Профиль</NavLink>
     </nav>
   )
